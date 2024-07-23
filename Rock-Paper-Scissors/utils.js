75e0b0a631bc6166018bb2a4366fce22204adeab
Grapsinho <92812712+Grapsinho@users.noreply.github.com>
2024-07-23 21:13:22 +0400

    Update utils.js

--- Rock-Paper-Scissors/utils.js
+++ Rock-Paper-Scissors/utils.js
@@ -9,32 +9,44 @@ export const gameThing = {
         return this.choices[Math.floor(Math.random() * this.choices.length)];
     },
     result: '',
-    scorePlayer: 0,
-    scoreComputer: 0,
 }
 
 const player_score = getElement('.player_score')
 const comp_score = getElement('.comp_score')
+const tieScore = getElement('.tie_score')
+
+let player_score3 = parseInt(localStorage.getItem('player_score1')) || 0;
+let comp_score3 = parseInt(localStorage.getItem('computer_score')) || 0;
+let tieScore3 = parseInt(localStorage.getItem('tie_score')) || 0;
+
+player_score.textContent = player_score3
+comp_score.textContent = comp_score3
+tieScore.textContent = tieScore3
 
 export function startPlay(playerMove) {
     const computerChoice = gameThing.computerMove()
+
     if (playerMove === computerChoice) {
         gameThing.result = "It's a draw!";
-        gameThing.scorePlayer++
-        gameThing.scoreComputer++
+        tieScore3++
     } else if (
         (playerMove === 'Rock' && computerChoice === 'Scissors') ||
         (playerMove === 'Paper' && computerChoice === 'Rock') ||
         (playerMove === 'Scissors' && computerChoice === 'Paper')
     ) {
         gameThing.result = "You win!";
-        gameThing.scorePlayer++
+        player_score3++
     } else {
         gameThing.result = "You lose!";
-        gameThing.scoreComputer++
+        comp_score3++
     }
 
-    player_score.textContent = gameThing.scorePlayer
-    comp_score.textContent = gameThing.scoreComputer
+    localStorage.setItem("player_score1", JSON.stringify(player_score3))
+    localStorage.setItem("computer_score", JSON.stringify(comp_score3))
+    localStorage.setItem("tie_score", JSON.stringify(tieScore3))
+
+    player_score.textContent = player_score3
+    comp_score.textContent = comp_score3
+    tieScore.textContent = tieScore3
     document.getElementById('result').textContent = `Computer chose ${computerChoice}. ${gameThing.result}`;
 }
